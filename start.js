47e0f19097e7f665d2e685cc52fee9daa92edcdc
rcalvo <rcalvo@tid.es>
2016-09-09 19:02:53 +0200

    Add Gruntfile, DevDependency and JsHint file

--- start.js
+++ start.js
@@ -1,8 +1,6 @@
-//Start.js
+// Start.js
 //
 // This is the first comment about start project MovieCrawler
-//
-// 
 
 var express = require('express');
 var app = express();
@@ -10,10 +8,10 @@ var app = express();
 app.set('port', (process.env.PORT || 8000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response){
-	response.send('Hello World!');
+app.get('/', function(request, response) {
+    response.send('Hello World!');
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+    console.log('Node app is running at localhost:' + app.get('port'));
 });
